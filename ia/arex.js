fc0a170f8fb980502abe123f57d80ee249c56d19
Anuvyx <anuvyx.ceo@gmail.com>
2025-03-10 10:19:08 -0500

    Arex.js y .css: Mejora en visualización de file-tag

--- ia/arex.js
+++ ia/arex.js
@@ -443,8 +443,31 @@
     if (isUser) {
       // Procesar contenido para mostrar: reemplazar saltos de línea y formatear etiquetas de archivos
       const formattedContent = content
-        .replace(/\n/g, '<br>')
-        .replace(/\[(.*?)\]/g, '<span class="file-tag">$1</span>');
+      .replace(/\n/g, '<br>')
+      .replace(/\[(.*?)\]/g, (match, fileName) => {
+        // Se extrae la extensión del archivo
+        const fileExtension = fileName.split('.').pop().toUpperCase();
+        // Si tuviésemos el tamaño del archivo, podríamos incluirlo; de lo contrario, se deja vacío o se calcula de otra forma.
+        const fileSize = ''; // O extraer el valor si está disponible
+        return `
+          <div class="file-tag">
+            <div class="file-icon">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                <polyline points="14 2 14 8 20 8"></polyline>
+              </svg>
+            </div>
+            <div class="file-details">
+              <div class="file-name">${fileName}</div>
+              <div class="file-meta">
+                <span class="file-type">${fileExtension}</span>
+                <span class="file-size">${fileSize}</span>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+
       messageDiv.innerHTML = formattedContent;
     } else {
       let processedContent = marked.parse(content);
